20b81f6d9272fb6f9c6ca8d366d50318690ca13b
Pyx-hash <damascojames94@gmail.com>
2026-01-04 00:37:55 +0800

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+function showGoogleAlert() {
+    alert("Google Login, Under Development!");
+}
+function showFacebookAlert() {
+    alert("Facebook Login, Under Development!");
+}
+function comingSoon() {
+    alert("Coming Soon!");
+}
